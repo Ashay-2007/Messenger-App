f3112c300db28c723a1b35a61ed99a9324894727
Ashay2007 <ashaywanjarkar123@gmail.com>
2021-02-04 11:29:34 +0530

    added @material-ui/core

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import {Button, FormControl, Input, InputLabel} from '@material-ui/core';
 import './App.css';
 
 function App() {
@@ -14,9 +15,15 @@ function App() {
     <div className="App">
       <h1>Hello Programmers !!</h1>
 
-      <input value={input} onChange={event => setInput(event.target.value)}/> 
-      <button onClick={sendMessage}>Send Message</button> 
-
+      <form action="">
+        <FormControl>
+          <InputLabel>Enter a message</InputLabel>
+          <Input value={input} onChange={event => setInput(event.target.value)} />
+          <Button disabled={!input} variant="contained" color="primary" type="submit" onClick={sendMessage}>Send Message</Button> 
+        </FormControl>
+      </form>
+      
+      
       {
         messages.map(message => (
           <p>{message}</p>
